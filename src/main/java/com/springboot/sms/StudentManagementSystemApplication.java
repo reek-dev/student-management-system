23ec3557fe245521fda4288f9610d96e7d487d3a
Reekdev Ray <ray.reekdev@gmail.com>
2023-03-15 21:33:05 +0530

    1. Created the list student functionality and built the UI using bootstrap. 2. Added validation for name fields by removing whitespace from inputs and making the fields titlecase by default.

--- src/main/java/com/springboot/sms/StudentManagementSystemApplication.java
+++ src/main/java/com/springboot/sms/StudentManagementSystemApplication.java
@@ -1,13 +1,36 @@
 package com.springboot.sms;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.springboot.sms.entity.Student;
+import com.springboot.sms.repository.StudentRepository;
+
 @SpringBootApplication
-public class StudentManagementSystemApplication {
+public class StudentManagementSystemApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementSystemApplication.class, args);
+	}
+	
+	@Autowired
+	private StudentRepository studentRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		/*
+		 * Student s1 = new Student("Reekdev", "Ray", "ray.reekdev@gmail.com");
+		 * studentRepository.save(s1);
+		 * 
+		 * Student s2 = new Student("Aman", "Yadav", "aman.yadav@gmail.com");
+		 * studentRepository.save(s2);
+		 * 
+		 * Student s3 = new Student("Rahul", "Singh", "singh.rahul@gmail.com");
+		 * studentRepository.save(s3);
+		 */
 	}
 
 }
--- src/main/java/com/springboot/sms/entity/Student.java
+++ src/main/java/com/springboot/sms/entity/Student.java
@@ -1,5 +1,8 @@
 package com.springboot.sms.entity;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,18 +13,18 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "students")
 public class Student {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "s_id")
 	private Long studentId;
-	
+
 	@Column(name = "s_first_name", nullable = false)
 	private String studentFirstName;
-	
+
 	@Column(name = "s_last_name", nullable = false)
 	private String studentLastName;
-	
+
 	@Column(name = "s_email", nullable = false)
 	private String studentEmail;
 
@@ -30,6 +33,7 @@ public class Student {
 	}
 
 	public void setStudentId(long studentId) {
+		System.out.println("Student class: inside setStudentId() setter.");
 		this.studentId = studentId;
 	}
 
@@ -38,7 +42,8 @@ public class Student {
 	}
 
 	public void setStudentFirstName(String studentFirstName) {
-		this.studentFirstName = studentFirstName;
+		System.out.println("Student class: inside setStudentFirstName() setter.");
+		this.studentFirstName = getTitleCase(studentFirstName.trim());
 	}
 
 	public String getStudentLastName() {
@@ -46,7 +51,8 @@ public class Student {
 	}
 
 	public void setStudentLastName(String studentLastName) {
-		this.studentLastName = studentLastName;
+		System.out.println("Student class: inside setStudentLastName() setter.");
+		this.studentLastName = getTitleCase(studentLastName.trim());
 	}
 
 	public String getStudentEmail() {
@@ -57,8 +63,8 @@ public class Student {
 		this.studentEmail = studentEmail;
 	}
 
-	public Student(long studentId, String studentFirstName, String studentLastName, String studentEmail) {
-		this.studentId = studentId;
+	public Student(String studentFirstName, String studentLastName, String studentEmail) {
+		System.out.println("Student class: inside parameterized constructor.");
 		this.studentFirstName = studentFirstName;
 		this.studentLastName = studentLastName;
 		this.studentEmail = studentEmail;
@@ -71,6 +77,23 @@ public class Student {
 	public String toString() {
 		return "Student [studentId=" + studentId + ", studentFirstName=" + studentFirstName + ", studentLastName="
 				+ studentLastName + ", studentEmail=" + studentEmail + "]";
+	}
+
+	// this method converts the names into title case
+	private static String getTitleCase(String text) {
+
+		if (text == null)
+			return null;
+
+		Pattern pattern = Pattern.compile("\\b([a-zÀ-ÖØ-öø-ÿ])([\\w]*)");
+		Matcher matcher = pattern.matcher(text.toLowerCase());
+
+		StringBuilder buffer = new StringBuilder();
+
+		while (matcher.find())
+			matcher.appendReplacement(buffer, matcher.group(1).toUpperCase() + matcher.group(2));
+
+		return matcher.appendTail(buffer).toString();
 	}
 
 }
